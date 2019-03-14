54699815704e2cb0e71193ceb4025e277837f8eb
Jessie <3180111435@zju.edu.cn>
2019-03-14 12:27:19 +0800

    practicum 4 revision

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -6,21 +6,7 @@ Created on Wed Mar 13 10:50:36 2019
 """
 
 #for simplicity, assume that x is no longer larger than 8129
-#first choose a number and find the biggest 2**n it contains,using function mode 2
-n=7680
-i=0
-answer="7680 is"
-while i<=13:
-    if n/2>=1:
-       n=n/2
-       i=i+1
-       answer=answer+"2**"+str(i)
-       n=7680-2**(i-1)     
-    else:
-       answer=answer+"2**0"
-
-
-
+#first choose a numben=7680r and find the biggest 2**n it contains,using function mode 2
 # from the former code we can know that the biggest i is 12
 #why this step cannot be calculated m=n-2**12
 
@@ -30,7 +16,7 @@ answer= "7680="
 while i>=0:
      if 2**i<m:
         m=m-2**i
-        answer=answer + "+2**"+str(i)
+        answer=answer + "+2**"+ str(i)
         i=i-1
      else:
         i=i-1
